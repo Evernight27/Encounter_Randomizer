11168760246567858993e9bf725e3bd3d3eef2a8
Marshall Grubb <marshallgrubb@Marshalls-MacBook-Air.local>
2023-02-06 12:36:51 -0500

    I have updated my project files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const charCreate = document.querySelector('.charCreate')
 const partyList = document.querySelector('#partyList')
 const CR = document.querySelector('#CR')
 const encounter = document.querySelector('#encounter')
-let crList = []
+let crList = {}
 
 charCreate.addEventListener('click', () => {
   createChar(levelInput.value)
@@ -18,20 +18,35 @@ charCreate.addEventListener('click', () => {
 })
 
 generate.addEventListener('click', async () => {
-  getCR(CR.innerHTML)
-  const monster = await randomMon(crList.results)
-  console.log(monster.name);
-  encounter.textContent = monster.name
+  if (CR.innerHTML === '') {
+    encounter.textContent = 'Create a character first.'
+  }
+  else {
+    const monsters = await getCR(CR.innerHTML)
+    const monster = await randomMon(monsters)
+    encounter.innerHTML = `
+    Monster: ${monster.name}<br />
+    Stats:
+    ${monster.strength} str,
+    ${monster.dexterity} dex,
+    ${monster.constitution} con,
+    ${monster.intelligence} int,
+    ${monster.wisdom} wis,
+    ${monster.charisma} cha<br />
+    HP: ${monster.hit_points}<br />
+    AC: ${monster.armor_class}<br />
+    Size: ${monster.size}<br />
+    Type: ${monster.type}<br />
+    Alignment: ${monster.alignment}<br />
+    Armor Type: ${monster.armor_desc}<br />`
+  }
 })
 
-async function randomMon(array) {
-  const randomElement = array[Math.floor(Math.random() * array.length)];
-  // console.log(randomElement);
-  return randomElement;
+async function randomMon(monsters) {
+  const monsterArray = Object.entries(monsters)
+  const randomMonster = monsterArray[Math.floor(Math.random() * monsterArray.length)];
+  return randomMonster[1];
 }
-
-//TODO 
-
 
 async function createChar(element) {
   const p = document.createElement('p')
@@ -50,6 +65,5 @@ async function getCR(cr) {
   const res = await fetch(`${BASE_URL}/?challenge_rating=${cr}`)
   crList = await res.json()
   console.log(crList.results);
+  return crList.results
 }
-// getCR(8)
-// console.log(crList);
